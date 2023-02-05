617d0b96fbcf03e5f4d202fb042bd3db2360c651
praveensam <psambath@unifonic.com>
2023-02-05 18:33:22 +0400

    Jira:  |   added files for ssl alert

---
+++ charts/sslalert/files/sam.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type (
+	result struct {
+		url_s, url   string
+		days         int
+		valid        bool
+		reachability bool
+	}
+
+	stats struct {
+		c     chan result
+		count int
+	}
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+func url_validator(url string) bool {
+	client := &http.Client{Timeout: 10 * time.Second}
+	_, err := client.Get("https://" + url)
+
+	if err == nil {
+
+		return true
+	} else {
+		return false
+	}
+
+	return true
+}
+
+func (s *stats) getdate(url string) {
+	defer wg.Done()
+
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	url_f := fmt.Sprintf("%s:443", (url))
+
+	conn, err := tls.Dial("tcp", url_f, conf)
+	if err != nil {
+		wg.Done()
+		//goto SAM
+		println("errorss")
+	}
+
+	defer conn.Close()
+	certs := conn.ConnectionState().PeerCertificates
+	//for _, cert := range certs {
+	cert := certs[0]
+	currentTime := time.Now()
+
+	d1, _ := time.Parse("2006-01-02 ", cert.NotAfter.Format("2006-01-02"))
+	d2, _ := time.Parse("2006-01-02 ", currentTime.Format("2006-01-02"))
+
+	if int(d1.Sub(d2).Hours()/24) > 50 {
+		s.c <- result{url: url, url_s: "Enough days", days: int(d1.Sub(d2).Hours() / 24), valid: true, reachability: true}
+		s.count = s.count + 1
+	} else {
+		s.c <- result{url: url, url_s: "Enough days", days: int(d1.Sub(d2).Hours() / 24), valid: false, reachability: true}
+		s.count = s.count + 1
+	}
+	//	}
+
+}
+
+func results(w http.ResponseWriter, req *http.Request) {
+
+	s := &stats{c: make(chan result, 1000)}
+	lists := []string{
+		"prometheus.observe.internal.unifonic.com",
+		"www.google.com",
+		"www.yahoo.com",
+		"www.unifonic.com",
+		"wwww.ddssss.com",
+		"www.sneouhfbakjd.com",
+	}
+	for _, i := range lists {
+
+		if url_validator(i) {
+			wg.Add(1)
+			go s.getdate(i)
+
+		} else {
+			prt := fmt.Sprintf("web_ssl{url=%s,days=%d,valid=%s,reachability=%s,reason=%s} = 0\n", i, 0, "false", "false", "Unable to reach the url")
+			fmt.Fprintf(w, prt)
+			println("run", i)
+		}
+	}
+	wg.Wait()
+	for i := 0; i < s.count; i++ {
+		select {
+		case d := <-s.c:
+			if d.valid && d.reachability {
+				prt := fmt.Sprintf("web_ssl{url=%s,days=%d,valid=%t,reachability=%t,reason=\"\"} = 1\n", d.url, d.days, d.valid, d.reachability)
+				fmt.Fprintf(w, prt)
+			} else {
+
+				prt := fmt.Sprintf("web_ssl{url=%s,days=%d,valid=%t,reachability=%t,reason=\"About to expire\"} = 0\n", d.url, d.days, d.valid, d.reachability)
+				fmt.Fprintf(w, prt)
+			}
+		}
+	}
+
+}
+
+func main() {
+
+	http.HandleFunc("/metrics", results)
+
+	http.ListenAndServe(":8090", nil)
+}
+
